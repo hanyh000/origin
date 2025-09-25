72573b9fb98677a89ba25d0008bc73e93409894c
hanyh <hanyh000@naver.com>
2025-09-25 14:25:38 +0900

    버튼식 로또추천코드 작성

--- tkinter-lotto/main.py
+++ tkinter-lotto/main.py
@@ -8,7 +8,7 @@ def buttonClick():
     print(random.sample(lotto_num,6))
 window=tkinter.Tk()
 window.title("lotto")
-window.geometry("400x200")
+window.geometry("1200x800")
 window.resizable(False,False)
 button = tkinter.Button(window, overrelief="solid",text="번호확인",width=15,command=buttonClick,repeatdelay=5000,repeatinterval=500,height=7)
 button.pack()
